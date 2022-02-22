e031248cf16418c5990768a08963a866ad8f989b
peka80 <petar.dragovic@gmail.com>
2022-02-22 22:11:54 +0100

    before and beforeEach

--- cypress/integration/telenor.spec.js
+++ cypress/integration/telenor.spec.js
@@ -1,10 +1,12 @@
 /// <reference types="Cypress" />
 
 describe("Testing Telenor.rs home page slider", () => {
-  // Implicite Assertation
-  it("Visit Telenor.rs website - Implicite", () => {
-    cy.visit("https://www.telenor.rs/");
+  beforeEach("Visit Telenor Homepage", () => {
+    cy.visit("https://www.telenor.rs/")
+  });
 
+  // Implicite Assertation
+  it("Assert Slider - Implicite", () => {
     cy.get("[aria-controls='navigation12']", { timeout: 15000 }).should(
       "have.class",
       "slick-active"
@@ -12,9 +14,7 @@ describe("Testing Telenor.rs home page slider", () => {
   });
 
   // Explicite Assertation
-  it("Visit Telenor.rs website - Explicite", () => {
-    cy.visit("https://www.telenor.rs/");
-
+  it("Assert Slider with hooks - Explicite", () => {
     cy.get("[aria-controls='navigation12']", { timeout: 15000 }).should(
       ($x) => {
         expect($x).to.have.class("slick-active");
